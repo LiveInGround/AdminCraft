1a8a6d6db07d2e3feecbeafaec8b4e027405c582
LiveinGround <129398571+LiveInGround@users.noreply.github.com>
2025-09-21 13:24:15 +0200

    Adding event blocking in SP
    - block interract
    - (entity interract)
    - block place
    - block left click
    - block break
    - PvP
    + effect system

--- src/main/java/fr/liveinground/my_server_utilities/Config.java
+++ src/main/java/fr/liveinground/my_server_utilities/Config.java
@@ -1,6 +1,7 @@
 package fr.liveinground.my_server_utilities;
 
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -10,6 +11,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
@@ -22,6 +24,9 @@ public class Config {
 
     private static final ForgeConfigSpec.BooleanValue ENABLE_SPAWN_PROTECTION = BUILDER.comment("Should the spawn protection being enabled ?")
             .define("enableSpawnProtection", true);
+
+    private static final ForgeConfigSpec.IntValue SP_OP_LEVEL = BUILDER.comment("The operator level required to bypass protections")
+            .defineInRange("bypassOPLevel", 1, 0, 4);
 
     private static final ForgeConfigSpec.IntValue SPAWN_PROTECTION_CENTER_X = BUILDER.comment("The X coordinate of the spawn protection")
             .defineInRange("spawnProtectionCenterX", 0, Integer.MIN_VALUE, Integer.MAX_VALUE);
@@ -48,6 +53,9 @@ public class Config {
     private static final ForgeConfigSpec.ConfigValue<List<? extends String>> ALLOWED_BLOCKS = BUILDER.comment("A list of blocks the players are allowed to interact to.")
             .defineListAllowEmpty("allowedBlocks", List.of("minecraft:stone_button"), Config::validateBlockName);
 
+    private static final ForgeConfigSpec.ConfigValue<List<? extends String>> SP_EFFECTS = BUILDER.comment("A list of effects applied in the spawn protection")
+            .defineListAllowEmpty("spawnProtectionEffects", List.of("minecraft:resistance", "minecraft:regeneration", "minecraft:saturation"), Config::validateEffectName);
+
     static final ForgeConfigSpec SPEC = BUILDER.build();
 
     public static boolean sp_enabled;
@@ -59,14 +67,22 @@ public class Config {
     public static String sp_leave_msg;
     public static boolean sp_pvp_enabled;
     public static Set<Block> allowedBlocks;
+    public static Set<MobEffect> sp_effects;
+    public static int sp_op_level;
+
 
     private static boolean validateBlockName(final Object obj) {
         return obj instanceof final String blockName && ForgeRegistries.BLOCKS.containsKey(new ResourceLocation(blockName));
     }
 
+    private static boolean validateEffectName(final Object obj) {
+        return obj instanceof final String effectName && ForgeRegistries.MOB_EFFECTS.containsKey(new ResourceLocation(effectName));
+    }
+
     @SubscribeEvent
-    static void onLoad(final ModConfigEvent event) {
+    static void onLoad(final ModConfigEvent.Loading event) {
         sp_enabled = ENABLE_SPAWN_PROTECTION.get();
+        sp_op_level = SP_OP_LEVEL.get();
         sp_center_x = SPAWN_PROTECTION_CENTER_X.get();
         sp_center_z = SPAWN_PROTECTION_CENTER_Z.get();
         sp_radius = SPAWN_PROTECTION_RADIUS.get();
@@ -77,5 +93,15 @@ public class Config {
         allowedBlocks = ALLOWED_BLOCKS.get().stream()
                 .map(blockName -> ForgeRegistries.BLOCKS.getValue(new ResourceLocation(blockName)))
                 .collect(Collectors.toSet());
+        sp_effects = SP_EFFECTS.get().stream()
+                .map(effectName -> ForgeRegistries.MOB_EFFECTS.getValue(new ResourceLocation(effectName)))
+                .collect(Collectors.toSet());
+    }
+
+    @SubscribeEvent
+    static void onReload(final ModConfigEvent.Reloading event) {
+        sp_effects.clear();
+        allowedBlocks.clear();
+        onLoad(null);
     }
 }
--- src/main/java/fr/liveinground/my_server_utilities/My_server_utilities.java
+++ src/main/java/fr/liveinground/my_server_utilities/My_server_utilities.java
@@ -1,7 +1,22 @@
 package fr.liveinground.my_server_utilities;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.ChatFormatting;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.event.level.BlockEvent;
+import net.minecraftforge.event.level.ExplosionEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -16,6 +31,7 @@ public class My_server_utilities {
 
     public static final String MODID = "my_server_utilities";
     private static final Logger LOGGER = LogUtils.getLogger();
+    private static final String SP_TAG = "inSpawnProtection";
 
     public My_server_utilities() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -26,7 +42,131 @@ public class My_server_utilities {
     }
 
     @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-        ;
+    public void onExplode(ExplosionEvent event) {
+        for (BlockPos pos: event.getExplosion().getToBlow()) {
+            if (isInSP(event.getLevel(), pos)) {
+                event.getExplosion().clearToBlow();
+                LOGGER.info("An explosion was cancelled in spawn protection.");
+            }
+        }
+    }
+
+    public static boolean isAllowed(Entity entity, Level level, BlockPos pos) {
+        if (!isInSP(level, pos)) return true;
+        return entity instanceof ServerPlayer sp && sp.hasPermissions(Config.sp_op_level);
+    }
+
+    @SubscribeEvent
+    public void onBlockInteract(PlayerInteractEvent.RightClickBlock e) {
+        Player player = e.getEntity();
+        ServerPlayer serverPlayer = (ServerPlayer) player;
+        BlockPos interactPos = e.getPos();
+        Level interactLevel = e.getLevel();
+
+        if (isAllowed(serverPlayer, interactLevel, interactPos)) return;
+
+        if (!Config.allowedBlocks.contains(interactLevel.getBlockState(interactPos).getBlock())) {
+            e.setCanceled(true);
+        }
+    }
+    /*
+    @SubscribeEvent
+    public void onEntityInteract(PlayerInteractEvent.EntityInteract e) {
+        Player player = e.getEntity();
+        ServerPlayer serverPlayer = (ServerPlayer) player;
+        BlockPos interactPos = e.getPos();
+        Level interactLevel = e.getLevel();
+
+        if (!isAllowed(serverPlayer, interactLevel, interactPos)) {
+            e.setCanceled(true);
+        }
+    }*/
+
+    @SubscribeEvent
+    public void onBlockPlace(BlockEvent.EntityPlaceEvent e) {
+        if (!(isAllowed(e.getEntity(), (Level) e.getLevel(), e.getPos()))) {
+            e.setCanceled(true);
+        }
+    }
+
+    @SubscribeEvent
+    public void onBlockLeftClick(PlayerInteractEvent.LeftClickBlock e) {
+        if (!(isAllowed(e.getEntity(), e.getLevel(), e.getPos()))) {
+            e.setCanceled(true);
+        }
+    }
+
+    @SubscribeEvent
+    public void onBlockBreak(BlockEvent.BreakEvent e) {
+        if (!(isAllowed(e.getPlayer(), (Level) e.getLevel(), e.getPos()))) {
+            e.setCanceled(true);
+        }
+    }
+
+    private static boolean isInSP(Level level, BlockPos pos) {
+        if (!Config.sp_enabled) return false;
+        if (level.dimension() == Level.OVERWORLD) {
+            int minX = Config.sp_center_x - Config.sp_radius;
+            int maxX = Config.sp_center_x + Config.sp_radius;
+            int minZ = Config.sp_center_z - Config.sp_radius;
+            int maxZ = Config.sp_center_z + Config.sp_radius;
+            return (pos.getX() >= minX && pos.getX() <= maxX && pos.getZ() >= minZ && pos.getZ() <= maxZ);
+        }
+        return false;
+    }
+
+    private static boolean isInSP(Entity entity) {
+        Level level = entity.level();
+        BlockPos pos = entity.getOnPos();
+        if (!Config.sp_enabled) return false;
+        if (level.dimension() == Level.OVERWORLD) {
+            int minX = Config.sp_center_x - Config.sp_radius;
+            int maxX = Config.sp_center_x + Config.sp_radius;
+            int minZ = Config.sp_center_z - Config.sp_radius;
+            int maxZ = Config.sp_center_z + Config.sp_radius;
+            return (pos.getX() >= minX && pos.getX() <= maxX && pos.getZ() >= minZ && pos.getZ() <= maxZ);
+        }
+        return false;
+    }
+
+    @SubscribeEvent
+    public void onPvP(AttackEntityEvent e) {
+        boolean targetCondition;
+        Entity target = e.getTarget();
+        Player attacker = e.getEntity();
+        BlockPos targetPos = target.getOnPos();
+        BlockPos attackerPos = attacker.getOnPos();
+
+        if (target instanceof Player) {
+            if (attacker.hasPermissions(Config.sp_op_level)) return;
+
+            if (isInSP(attacker) || isInSP(target)) {
+                e.setCanceled(true);
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public void onPlayerMove(TickEvent.PlayerTickEvent e) {
+        Player player = e.player;
+        ServerPlayer serverPlayer = (ServerPlayer) player;
+        if (isInSP(player.level(), player.getOnPos())) {
+            for (MobEffect effect: Config.sp_effects) {
+                player.addEffect(new MobEffectInstance(effect, Integer.MAX_VALUE, 255, false, false));
+            }
+            if (player.getTags().contains(SP_TAG)) return;
+            else {
+                player.addTag(SP_TAG);
+                serverPlayer.displayClientMessage(Component.literal(Config.sp_enter_msg).withStyle(ChatFormatting.GREEN), true);
+            }
+        } else {
+            if (player.getTags().contains(SP_TAG)){
+                player.removeTag(SP_TAG);
+                for (MobEffect effect: Config.sp_effects) {
+                    e.player.removeEffect(effect);
+                }
+                serverPlayer.displayClientMessage(Component.literal(Config.sp_leave_msg).withStyle(ChatFormatting.RED), true);
+            }
+        }
     }
 }
