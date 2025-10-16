ec42565da8757f8b6b7b8124b51f9b4df2866808
LiveinGround <129398571+LiveInGround@users.noreply.github.com>
2025-10-16 14:41:14 +0200

    Adding mixin to fix client crash on respawn

---
+++ src/main/java/fr/liveinground/admin_craft/mixins/PlayerRespawnMixin.java
@@ -0,0 +1,35 @@
+package fr.liveinground.admin_craft.mixins;
+
+import fr.liveinground.admin_craft.Config;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.players.PlayerList;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlayerList.class)
+public class PlayerRespawnMixin {
+    @Inject(
+            method = "respawn",
+            at = @At("HEAD")
+    )
+    private void onRespawn(ServerPlayer player, boolean keepEverything, CallbackInfoReturnable<ServerPlayer> cir) {
+        ServerLevel overworld = player.getServer().overworld();
+        BlockPos current = player.getRespawnPosition();
+        BlockPos defaultSpawn = overworld.getSharedSpawnPos();
+
+        if ((current == null || current.equals(defaultSpawn)) && Config.spawn_override) {
+            player.setRespawnPosition(
+                    Level.OVERWORLD,
+                    new BlockPos(Config.spawn_x, Config.spawn_y, Config.spawn_z),
+                    0f,
+                    true,
+                    false
+            );
+        }
+    }
+}
