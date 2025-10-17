d50f5256de2937498996b1074979c9a8930ddab3
LiveinGround <129398571+LiveInGround@users.noreply.github.com>
2025-10-17 17:34:39 +0200

    Fix tempban command syntax

--- src/main/java/fr/liveinground/admin_craft/commands/moderation/TempBanCommand.java
+++ src/main/java/fr/liveinground/admin_craft/commands/moderation/TempBanCommand.java
@@ -2,6 +2,7 @@ package fr.liveinground.admin_craft.commands.moderation;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
 import fr.liveinground.admin_craft.Config;
 import fr.liveinground.admin_craft.moderation.CustomSanctionSystem;
 import fr.liveinground.admin_craft.moderation.SanctionConfig;
@@ -14,26 +15,29 @@ import net.minecraft.server.level.ServerPlayer;
 
 public class TempBanCommand {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
-
         dispatcher.register(Commands.literal("tempban")
-                .requires(commandSource -> commandSource.hasPermission(Config.tempban_level))
-                .then(Commands.argument("player", EntityArgument.player()))
-                .then(Commands.argument("duration", StringArgumentType.word()))
-                        .executes(ctx -> {
-                            ServerPlayer sanctionedPlayer = EntityArgument.getPlayer(ctx, "player");
-                            String reason = "Banned by an operator";
-                            String duration = StringArgumentType.getString(ctx, "duration");
-                            CustomSanctionSystem.banPlayer(ctx.getSource().getServer(), ctx.getSource().getTextName(), sanctionedPlayer, reason, SanctionConfig.getDurationAsDate(duration));
-                            ctx.getSource().sendSuccess(() -> Component.literal("Banned " + sanctionedPlayer.getDisplayName().getString() + " " + duration + ": " + reason), true);
-                            return 1;
-                        })
-                .then(Commands.argument("reason", StringArgumentType.word()).executes(ctx -> {
-                    ServerPlayer sanctionedPlayer = EntityArgument.getPlayer(ctx, "player");
-                    String reason = StringArgumentType.getString(ctx, "reason");
-                    String duration = StringArgumentType.getString(ctx, "duration");
-                    CustomSanctionSystem.banPlayer(ctx.getSource().getServer(), ctx.getSource().getTextName(), sanctionedPlayer, reason, SanctionConfig.getDurationAsDate(duration));
-                    ctx.getSource().sendSuccess(() -> Component.literal("Banned " + sanctionedPlayer.getDisplayName().getString() + " " + duration + ": " + reason), true);
-                    return 1;
-                })));
+                        .requires(commandSource -> commandSource.hasPermission(Config.tempban_level))
+                                .then(Commands.argument("player", EntityArgument.player())
+                                        .then(Commands.argument("duration", StringArgumentType.word())
+                                                .executes(ctx -> {
+                                                    ServerPlayer sanctionedPlayer = EntityArgument.getPlayer(ctx, "player");
+                                                    String reason = "Banned by an operator";
+                                                    String duration = StringArgumentType.getString(ctx, "duration");
+                                                    tempban(ctx, sanctionedPlayer, duration, reason);
+                                                    return 1;
+                                                }))
+                                        .then(Commands.argument("reason", StringArgumentType.greedyString())
+                                                .executes(ctx -> {
+                                                    ServerPlayer sanctionedPlayer = EntityArgument.getPlayer(ctx, "player");
+                                                    String reason = StringArgumentType.getString(ctx, "reason");
+                                                    String duration = StringArgumentType.getString(ctx, "duration");
+                                                    tempban(ctx, sanctionedPlayer, duration, reason);
+                                                    return 1;
+                                                }))));
+    }
+
+    private static void tempban(CommandContext<CommandSourceStack> ctx, ServerPlayer player, String duration, String reason) {
+        CustomSanctionSystem.banPlayer(ctx.getSource().getServer(), ctx.getSource().getTextName(), player, reason, SanctionConfig.getDurationAsDate(duration));
+        ctx.getSource().sendSuccess(() -> Component.literal("Banned " + player.getDisplayName().getString() + " " + duration + ": " + reason), true);
     }
 }
